cafabc9017945202499297cd996e0f2f393f5afb
ykky <duclayan.dominique@gmail.com>
2020-09-16 01:39:54 +0400

    added edits

--- pigGame/app.js
+++ pigGame/app.js
@@ -1,14 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 const globalScore = document.getElementsByClassName('player-score');
 const curScore = document.getElementsByClassName('player-current-score');
 const dice = document.getElementById('dice');
@@ -20,75 +9,73 @@ var toAdd = 0;
 var player = 0;
 var globeScore = [0,0];
 
+addDice()
+
+function addDice(){
+    let newDice = document.createElement('img')
+    let parentNode = document.getElementsByTagName('img').parentElement
+    newDice.setAttribute('src','dice-6.png')
+    newDice.setAttribute('class', 'dice' )
+    console.log(newDice)
+    console.log(parentNode)
+}
 function rollDice(player){
     var diceFace = (Math.floor(Math.random()*(6-1)) + 1);
     img.src = `dice-${diceFace}.png`;
     console.log(diceFace);
     
     if(diceFace === 1){                         //if rolled a 1, current score is reset to 0 on display and toAdd value to 0. switches players.
-        toAdd = 0;
-        curScore[player].textContent = toAdd;
+        curScore[player].textContent = 0;
         player = switchPlayer(player);
     }else{
-        addScore(diceFace,player);              //else goes into addscore function
+        curScore[player].textContent = (toAdd += diceFace);
     }
 return player;
-}
-
-function addScore(diceFace,player){
-    //adds randomized dice value to current and displays it
-    
-    toAdd += diceFace;
-    curScore[player].textContent = toAdd;
 }
 
 function holdScore(player,toAdd){
     //adds the current displayed score to players' global score and displays it.
-
-    globeScore[player] = globeScore[player] + toAdd;
-    globalScore[player].textContent = globeScore[player];
-  
+    globalScore[player].textContent = (globeScore[player] += toAdd); 
 return player;
 }
 
 function switchPlayer(player){
-//changes which player is currently active and also tells the game which player is to move next
-
-var current = document.querySelector('.player-'+player+'-panel');
-current.className = 'player-'+player+'-panel';
-
-    if (player === 0){
-        player = 1;
-        current = document.querySelector('.player-'+player+'-panel');
-        current.className = 'player-'+player+'-panel active';
-    }
-    else{
-        player = 0;
-        current = document.querySelector('.player-'+player+'-panel');
-        current.className = 'player-'+player+'-panel active';
-    }
-
+    // remove active to previous player
+    document.querySelector(`.player-${player}-panel`).className = `player-${player}-panel`;  
+    //if player =0 then change to 1 else change to 0
+    player = ( player === 0 ) ? 1 : 0;
+    //add active to current player
+    document.querySelector(`.player-${player}-panel`).className = `player-${player}-panel active`;
 return player;
 }
 
 function winCondition(player,globeScore){
-var win = null;
-
-    if(globeScore[player] >= 100){              //check if score reach or exceeded 100
-        win = player;                           //assigns player currently being checked as winner if exceeded or reached 100
-    }
-
-return win;
+    var win = null;
+        if(globeScore[player] >= 10){              //check if score reach or exceeded 100
+            win = player;
+            displayWin(win)                           //assigns player currently being checked as winner if exceeded or reached 100
+        }
+    return win;
 }
 
-function displayWin(win){                       //display winner
+function displayWin(win){        
+    document.getElementById(`name-${win}`).innerHTML = `Player ${win+1} Won`
+    setTimeout(newGame,1000)
+}
 
-    if (win === 0){
-        //display win player 1
-    }else
-    if (win === 1){
-        //display win player 2
+function newGame(){
+    console.log('New game')
+    win = null;
+    toAdd = 0;                                                              //resets toAdd score on current to 0
+        globalScore[0].parentElement.className = 'player-0-panel active'
+        globalScore[1].parentElement.className = 'player-1-panel'
+        
+    for(let i=0;i<2;i++){             
+        document.getElementById(`name-${i}`).innerHTML = `Player ${i+1}`                        //resets all global scores to 0 as well as displays
+        globalScore[i].textContent = '0';
+        curScore[i].textContent = '0';
     }
+
 }
 
 document.addEventListener('click', e => {
@@ -103,26 +90,11 @@ document.addEventListener('click', e => {
         toAdd = 0;                              //reset to Add to 0
         curScore[player].textContent = toAdd;   //reset current score display to 0
         win = winCondition(player,globeScore);  //check winCondition
-        displayWin(win);                        //display if winner
+        // displayWin(win);                        //display if winner
         player = switchPlayer(player);          //switch players
     }
 
     if (button === 'btn-new'){
-        win = null;
-        toAdd = 0;                                                              //resets toAdd score on current to 0
-        player = 1;                                                             //resets player 2's status to " "
-            current = document.querySelector('.player-'+player+'-panel');
-            current.className = 'player-'+player+'-panel';
-        player = 0;                                                             //resets player 1's status to active
-            current = document.querySelector('.player-'+player+'-panel');
-            current.className = 'player-'+player+'-panel active';
-
-        for(let i=0;i<2;i++){                                                   //resets all global scores to 0 as well as displays
-            globeScore[i] = 0;
-            globalScore[i].textContent = '0';
-            curScore[i].textContent = '0';
-        }
-
+        newGame()
     }
 })
-
